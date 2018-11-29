a389c168078d1e16eb77de3177044454b05a7561
Júlio César <45316537+JulioCzar777@users.noreply.github.com>
2018-11-29 15:14:15 -0200

    Update Questão 7.py

--- Questão 7.py
+++ Questão 7.py
@@ -21,3 +21,21 @@ for i in range(10):
 print(vetor1)
 print(vetor2)
 print(vetor3)
+#Vetores definidos pelo usuario
+vetor1 = []
+vetor2 = []
+vetor3 = []
+print("Primeiro vetor: ")
+for i in range(20):
+  if i <= 9:
+    vetor1.append(int(input()))
+  else:
+    if i == 10:
+      print('Segundo vetor: ')
+    vetor2.append(int(input()))
+for i in range(10):
+  vetor3.append(vetor1[i])
+  vetor3.append(vetor2[i])
+print(vetor1)
+print(vetor2)
+print(vetor3)
